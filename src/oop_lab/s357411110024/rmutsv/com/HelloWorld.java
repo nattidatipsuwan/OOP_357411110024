1da7005543a24f0c46800896f0e9e269059d11f9
nattidatipsuwan <nattide@hotmail.com>
2017-11-19 15:05:38 +0700

    Initial commit

---
+++ src/oop_lab/s357411110024/rmutsv/com/HelloWorld.java
@@ -0,0 +1,14 @@
+package oop_lab.s357411110024.rmutsv.com;
+
+public class HelloWorld {
+
+//            create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello World, I'am NAT");
+
+}// main
+
+
+
+}// class

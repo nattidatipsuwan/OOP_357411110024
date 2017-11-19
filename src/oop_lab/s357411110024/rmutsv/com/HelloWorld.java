0404211c1be230f1152c8e4d0e0791271d612a2b
nattidatipsuwan <nattide@hotmail.com>
2017-11-19 15:22:28 +0700

    Edit information

--- src/oop_lab/s357411110024/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s357411110024/rmutsv/com/HelloWorld.java
@@ -7,8 +7,8 @@ public static void main(String[] args) {
 
     System.out.println("Hello World, I'am NAT");
     System.out.println("nattida Tipsuwan");
-    System.out.println("357411110024");
-    System.out.println("is441");
+    System.out.println("357411110024,MT rmutsv");
+
 
 }// main
 

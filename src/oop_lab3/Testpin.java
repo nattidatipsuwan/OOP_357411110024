a6895bf03d215410202e308c19dc9c8df67a9ebb
nattidatipsuwan <nattide@hotmail>
2017-12-12 11:44:33 +0700

    Test pin

--- src/oop_lab3/Testpin.java
+++ src/oop_lab3/Testpin.java
@@ -1,4 +1,25 @@
 package oop_lab3;
 
+import java.util.Scanner;
+
 public class Testpin {
-}
+    public static void main(String[] args) {
+        String PIN ="1234",input;
+        int count = 0 ;
+        Scanner Scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter PIN :");
+            input =Scanner.nextLine();
+            if (PIN.equals(input)) {
+                System.out.println("welcome.");
+                break;
+            }else {
+                count++;
+                if (count==3){
+                    System.out.println("Locked");
+                    break;
+                }
+            }
+        }   while (count<=3) ;
+    }//main
+}//class

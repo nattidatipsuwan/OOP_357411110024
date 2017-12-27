c5ae23807cf709f2d52e2b259758fa8d57c69160
nattidatipsuwan <nattide@hotmail.com>
2017-12-27 16:07:41 +0700

    Sorting data in max min

--- src/oop_lab5/InputDataToArray.java
+++ src/oop_lab5/InputDataToArray.java
@@ -1,5 +1,7 @@
 package oop_lab5;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 //1.input data from user
@@ -11,14 +13,45 @@ public class InputDataToArray {
     private static final int MAX = 10;
 
     public static void main(String[] args) {
-        int num[]=new int[MAX];
+        Integer num[]=new Integer[MAX];
         num = inputData(num);
         ShowData(num);
         summation(num);
+        findMax(num);
+        finMin (num);
+        DescendingOrder(num);
+        AscendingOrder(num);
 
     }//main
 
-    private static void summation (int[]num) {
+    private static void AscendingOrder(Integer[] num) {
+        Arrays.sort(num,Collections.reverseOrder());
+       System.out.println("Ascending order");
+        ShowData(num);
+    }
+
+    private static void DescendingOrder(Integer[] num) {
+        Arrays.sort(num);
+        System.out.println("Descending Order");
+        ShowData(num);
+    }
+
+    private static void finMin(Integer[] num) {
+        System.out.println("The maximum value is : " + (Collections.min(Arrays.asList(num))));
+    }
+
+    private static void findMax(Integer[] num) {
+        System.out.println("The maximum value is : " + (Collections.max(Arrays.asList(num))));
+        //max value with basic code
+        Integer max=num[0];
+        for (int i = 0 ; i<num.length;i++){
+            if (max<=num[i]){
+                max=num[i];
+            }
+        }
+    }
+
+    private static void summation (Integer[]num) {
         int total = 0 ;
         for (int i=0;i<num.length;i++){
             total += num[i];
@@ -29,13 +62,13 @@ public class InputDataToArray {
     }
 
 
-    private static void ShowData(int[] num) {
+    private static void ShowData(Integer[] num) {
         System.out.println("Data in array");
         for (int val : num)
             System.out.print(val+" ");
     }
 
-    private static int[] inputData(int [ ]num) {
+    private static Integer[] inputData(Integer[ ]num) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Plese enter data to array : ");
         for (int i=0 ; i<num.length;i++){

bc6e0d9603fc588e4b7740788088a4b1d364e443
nattidatipsuwan <nattide@hotmail.com>
2018-01-23 09:12:05 +0700

    Class and object :student

---
+++ src/oop_lab7/Student.java
@@ -0,0 +1,59 @@
+package oop_lab7;
+//Talking about Student?
+//1.student ID
+//2.Major
+//3.Faculty
+//4.Name
+public class Student {
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", major='" + major + '\'' +
+                ", faculty='" + faculty + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }//to string
+
+    private String id;
+    private String major;
+    private String faculty;
+    private String name;
+    //constructor
+    //Default contructor
+    public Student(){}
+    //create constructor by owner
+    public  Student(String id,String m,String f, String n){
+        //assign data to class properties
+        this.id = id;
+        this.faculty = f;
+        this.major = m;
+        this.name = n;
+    }
+    //getter and setter methods
+
+    public  String  getID(){
+        return this.id;
+    }
+    public  void setID(String id){
+        this.id = id;
+    }
+    public  String getMajor(){
+        return  this.major;
+    }
+    public void setMajor(){
+        this.major=major;
+    }
+    public  String getFaculty() {
+        return this.faculty;
+    }
+    public void setFaculty(){
+        this.faculty=faculty;
+    }
+    public  String getName() {
+        return this.name;
+    }
+    public void setName(){
+        this.name = name;
+    }
+}//cless
